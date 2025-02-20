cda0bfa8a9fa89e9847b68d059e42ba34d86f8b0
Cabactulan-Virgil <cabactulanvirgil2021@gmail.com>
2025-02-20 15:10:29 +0800

    commit 1st changes

---
+++ Pages/api/imported.js
@@ -0,0 +1,26 @@
+export function Indexheader() {
+  return (
+    <div>
+      <h1>Index Header</h1>
+      <p>Welcome to the homepage!</p>
+    </div>
+  );
+}
+
+export function Indexfooter() {
+    return (
+        <div>
+        <h1>Index Footer</h1>
+        <p>Footer Section!</p>
+        </div>
+    );
+}
+
+export function Indexsidebar() {
+    return (
+        <div>
+        <h1>About IndexSidebar</h1>
+        <p>See My Content</p>
+        </div>
+    );
+}
